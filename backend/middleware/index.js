aad05e289ac905160732b8ae19a911a4485181d5
Rilwan Adams <adamzrealone@gmail.com>
2022-04-09 15:43:21 +0100

    bug: console.log removed

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -14,7 +14,6 @@ const errorMiddleware = async (ctx, next) => {
   try {
     await next();
   } catch (e) {
-    console.log(e);
     ctx.status = 400;
 
     ctx.body = new BaseResponse({ message: 'Something went wrong' });
