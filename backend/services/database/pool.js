d8145d8cefd134df43c15bfed25520dcc4656b81
Rilwan Adams <adamzrealone@gmail.com>
2022-04-09 10:24:19 +0100

    feat: selectAll renmaed to query

--- backend/services/database/pool.js
+++ backend/services/database/pool.js
@@ -3,8 +3,8 @@ const { databaseConfig } = require('../../config');
 
 const pool = new Pool(databaseConfig);
 
-const selectAll = async (sql, args) => {
+const query = async (sql, args) => {
   return (await pool.query(sql, args)).rows;
 };
 
-module.exports = { selectAll };
+module.exports = { query };
