81652998d44c018f9e564fbf0c4bb095daaf30e9
Rilwan Adams <adamzrealone@gmail.com>
2022-04-09 08:22:47 +0100

    feat: query endpoint created

--- backend/route/index.js
+++ backend/route/index.js
@@ -1,8 +1,25 @@
 const router = require('@koa/router')();
+const { BaseResponse } = require('../models');
+const { searchMovies } = require('../services');
 
-router.get('/', (ctx, next) => {
-  ctx.status = 200;
-  ctx.body = 'Hello World';
+router.get('/', async (ctx, next) => {
+  try {
+    let res = [];
+    // TODO: get query from endpoint: ctx.request.query
+    const { q } = ctx.request.query;
+
+    // TODO: search database for such data
+    if (q) res = await searchMovies(q);
+
+    // TODO return data from database
+    ctx.status = 200;
+    ctx.body = new BaseResponse({ data: res });
+  } catch (error) {
+    console.log(error);
+    ctx.status = 400;
+    ctx.body = new BaseResponse({ message: 'Something went wrong' });
+  }
+
   next();
 });
 
